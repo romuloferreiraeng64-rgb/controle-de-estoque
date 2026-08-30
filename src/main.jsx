66f3de4b55dc4c2036ad2e5873245ae7a90bae05
romuloferreiraeng64-rgb <romuloferreiraeng64@gmail.com>
2026-08-29 22:12:29 -0300

    Create main.jsx

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "../estoque-limpeza-industrial.jsx";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
